69a845f0d19b2fb5d53274d0501d95f7094a880f
Düzgün Tutar <duzguntutar@gmail.com>
2020-09-21 15:03:33 +0300

    Update expressions.cs

--- 1-c#/expressions.cs
+++ 1-c#/expressions.cs
@@ -1,1 +1,15 @@
 
+Func<VpPaperWorkIntDocumentTracking, bool> funcFirstFilter = (d) =>
+{
+    var divisionCode = "";
+
+    if (!string.IsNullOrEmpty(d.RecordCode))
+    {
+        var array = d.RecordCode.Split('_');
+        if (array?.Length > 0)
+            divisionCode = array[1];
+    }
+
+    return d.AppName.Equals(request.App.ToString()) && divisionCode.Equals(transactionHeader.PerformerUser.DivisionCode.ToString());
+};
+dataList = context.VpPaperWorkIntDocumentTracking.Where(funcFirstFilter).ToList();
